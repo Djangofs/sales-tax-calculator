3c8ed9a093e17f970bf2c5bb163161c276fcbc41
djangofs <djangoshelton@gmail.com>
2024-06-11 13:48:21 +0100

    add input mechanism and main program file

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { promises as fsPromises } from "fs";
+
+const FILE_PATH = "./inputs";
+const OUTPUT_PATH = "./outputs";
+
+const readFiles = async (filePath: string): Promise<string[]> => {
+  try {
+    const files = await fsPromises.readdir(filePath);
+    return files;
+  } catch (err) {
+    console.error(`🔴 Error reading files in ${filePath}`, err);
+  }
+  return [];
+};
+
+const main = async (): Promise<void> => {
+  console.log("Start reading files...");
+
+  const files = await readFiles(FILE_PATH);
+
+  if (files.length === 0) {
+    console.log("🔴 No files found");
+    return;
+  }
+
+  console.log(`🟢 Found ${files.length} files in ${FILE_PATH}`);
+
+  for (const file of files) {
+    console.log(`🟢 Reading file ${file}`);
+    const data = await fsPromises.readFile(`${FILE_PATH}/${file}`, "utf-8");
+
+    // Do something with the data
+    // Replace data with processed version
+    console.log(`🟢 Processing file ${file}`);
+    const newFileName = `${OUTPUT_PATH}/output_${file}`;
+    await fsPromises.writeFile(`${newFileName}`, data);
+    console.log(`🟢 File ${file} processed and saved in ${newFileName}`);
+  }
+};
+
+main();
